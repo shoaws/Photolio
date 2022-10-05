27e658270a45a2946304c9bfd10340e08c55b1c1
shoaws <108819031+shoaws@users.noreply.github.com>
2022-10-05 11:06:22 +0000

    [Add]新規投稿機能

--- app/controllers/public/photos_controller.rb
+++ app/controllers/public/photos_controller.rb
@@ -1,10 +1,31 @@
 class Public::PhotosController < ApplicationController
+  
   def new
-    
+    @photo = Photo.new
   end
+  
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.member_id = current_member.id
+    if @photo.save
+      redirect_to photo_path(@photo)
+    else
+      redirect_to request.referer
+    end
+  end
+  
   def index
   end
 
   def show
+  end
+  
+  def destroy
+  end
+  
+  private
+  
+  def photo_params
+    params.require(:photo).permit(:image, :body, :address, :member_id)
   end
 end
