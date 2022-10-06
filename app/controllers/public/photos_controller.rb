6126a05cf6370b9f093ecb946ba1332dd8ee4d67
shoaws <108819031+shoaws@users.noreply.github.com>
2022-10-06 03:59:43 +0000

    [Add]投稿編集・削除機能

--- app/controllers/public/photos_controller.rb
+++ app/controllers/public/photos_controller.rb
@@ -1,9 +1,9 @@
 class Public::PhotosController < ApplicationController
-  
+
   def new
     @photo = Photo.new
   end
-  
+
   def create
     @photo = Photo.new(photo_params)
     @photo.member_id = current_member.id
@@ -13,7 +13,7 @@ class Public::PhotosController < ApplicationController
       redirect_to request.referer
     end
   end
-  
+
   def index
     @photos = Photo.all
   end
@@ -22,12 +22,25 @@ class Public::PhotosController < ApplicationController
     @photo = Photo.find(params[:id])
     @member = @photo.member
   end
-  
-  def destroy
+
+  def edit
+    @photo = Photo.find(params[:id])
   end
-  
+
+  def update
+    photo = Photo.find(params[:id])
+    photo.update(photo_params)
+    redirect_to photo_path(photo)
+  end
+
+  def destroy
+    photo = Photo.find(params[:id])
+    photo.destroy
+    redirect_to member_path(current_member)
+  end
+
   private
-  
+
   def photo_params
     params.require(:photo).permit(:image, :body, :address, :member_id)
   end
