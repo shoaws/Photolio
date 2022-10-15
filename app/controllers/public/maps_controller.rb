67a5eaf99045f0f13cabf1549bf4b68803c2a0cc
shoaws <108819031+shoaws@users.noreply.github.com>
2022-10-15 04:20:37 +0000

    [Fix]地図表示ページレイアウト

--- app/controllers/public/maps_controller.rb
+++ app/controllers/public/maps_controller.rb
@@ -1,6 +1,8 @@
 class Public::MapsController < ApplicationController
   def index
     @photo = Photo.find(params[:photo_id])
+    @member = @photo.member
+    @another_photos = Photo.where(address: @photo.address).where.not(id: @photo.id).where.not(member_id: current_member.id).order("RANDOM()").limit(3)
   end
   
 end
