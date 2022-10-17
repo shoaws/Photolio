9068c451ea1cdd7419b40d852c68d6ce58b5693f
shoaws <108819031+shoaws@users.noreply.github.com>
2022-10-17 12:32:19 +0000

    [Fix]デプロイ、jpgファイル名修正

--- app/models/member.rb
+++ app/models/member.rb
@@ -26,8 +26,8 @@ class Member < ApplicationRecord
   # アイコン表示
   def get_profile_image
     unless profile_image.attached?
-      file_path = Rails.root.join('app/assets/images/no_image.jpeg')
-      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpeg', content_type: 'image/jpeg')
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
     end
     profile_image
   end
