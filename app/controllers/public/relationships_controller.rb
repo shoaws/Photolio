90d017073006c0b15f48012583c949a395fa2f6f
shoaws <108819031+shoaws@users.noreply.github.com>
2022-10-18 04:07:58 +0000

    [Fix]デプロイ、アイコン修正

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -13,12 +13,18 @@ class Public::RelationshipsController < ApplicationController
 
   def followings
     @member = Member.find(params[:member_id])
+    if @member.email == "guest@example.com"
+      redirect_to member_path(current_member)
+    end
     @members = @member.followings
     @random_members = Member.where.not(email: "guest@example.com").where.not(id: current_member.id).where.not(id: @members.ids).order("RANDOM()").limit(5)
   end
 
   def followers
     @member = Member.find(params[:member_id])
+    if @member.email == "guest@example.com"
+      redirect_to member_path(current_member)
+    end
     @members = @member.followers
   end
 
