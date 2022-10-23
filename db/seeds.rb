510cc167c98b82250689c94000a686505be2fb08
shoaws <108819031+shoaws@users.noreply.github.com>
2022-10-23 08:10:26 +0000

    [Add]テストデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ActiveStorage::AnalyzeJob.queue_adapter = :inline
+ActiveStorage::PurgeJob.queue_adapter = :inline
 
 Admin.create!(
     email: 'admin@admin',
