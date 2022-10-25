08e4183d8d3565ae65492f2b404520ca5112e23a
shoaws <108819031+shoaws@users.noreply.github.com>
2022-10-25 04:44:06 +0000

    [Fix]新規投稿、編集画面レイアウト調整

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,12 +2,12 @@ class Admin::HomesController < ApplicationController
   before_action :authenticate_admin!
 
   def top
-    @members = Member.page(params[:page]).per(10)
+    @members = Member.page(params[:page]).per(6)
   end
 
   def search
     @search_members = Member.admin_search(params[:keyword]).where(is_deleted: params[:is_deleted_search])
-    @members = @search_members.page(params[:page]).per(10)
+    @members = @search_members.page(params[:page]).per(6)
     @member_keyword = params[:keyword]
     if params[:is_deleted_search] == 'false'
       @is_deleted_search = "有効"
