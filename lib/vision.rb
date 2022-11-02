a00f73759ad1aee4f1fd522462abc4135829d9bc
shoaws <108819031+shoaws@users.noreply.github.com>
2022-11-02 06:57:38 +0000

    [Fix]Vision機能

--- lib/vision.rb
+++ lib/vision.rb
@@ -35,11 +35,11 @@ module Vision
       response = https.request(request, params)
       response_body = JSON.parse(response.body)
       # APIレスポンス出力
-      # if (error = response_body['responses'][0]['error']).present?
-      #   raise error['message']
-      # else
+      if (error = response_body['responses'][0]['error']).present?
+        raise error['message']
+      else
         response_body['responses'][0]['labelAnnotations'].pluck('description').take(3)
-      # end
+      end
     end
   end
 end
